30292d8a39993af814101f002a97c803d91d8447
Kavya1957 <kavyamnayak19@gmail.com>
2026-08-16 12:16:34 +0530

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class input
+{
+    Public static void main(String[]args)
+    {
+        Scanner sc = new Scanner (System.in);
+        int a = sc.nxtInt();
+        System.out.println("the number is +a");
+    }
+}
