222871e2e129d4563d1c14617b098b2af9ac864f
Kavya M <kavyamnayak19@gmail.com>
2026-08-23 16:49:27 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,19 @@
+class Array 
+{
+    public static void main(String[]args)
+    {
+        int a[]=new int[5];
+        a[0]=11;
+        a[1]=22;
+        a[2]=33;
+        a[3]=44;
+        a[4]=55;
+
+        System.out.println("ARRAY ELEMENTS ARE:");
+        for(int i=0;i<5;i++)
+        {
+            System.out.println(a[i]);
+        }
+    }
+    
+}
