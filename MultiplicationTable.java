45a250a352435cd2bcdce39a074d2ff499409d95
Kavya M <kavyamnayak19@gmail.com>
2026-08-25 12:41:08 +0530

    Add files via upload

--- MultiplicationTable.java
+++ MultiplicationTable.java
@@ -1,5 +1,5 @@
  import java.util.*;
-class LargestNumber{
+class MultiplicationTable{
     public static void main(String[]args){
           Scanner sc = new Scanner(System.in);
           System.out.println("Enter the number");
